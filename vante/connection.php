f21bb3cd4e579e836849f00cd9708bed9f96302f
natixvi <56030665+natixvi@users.noreply.github.com>
2023-03-22 18:22:16 +0100

    Update connection.php

--- vante/connection.php
+++ vante/connection.php
@@ -1,6 +1,6 @@
 <?php
     try{
-    $pdo = new PDO("mysql:host=localhost;port=3306;dbname=vante;charset=utf8", 'root','Stronka#95',[
+    $pdo = new PDO("mysql:host=localhost;port=3306;dbname=vante;charset=utf8", 'root','haslo',[
         PDO::ATTR_EMULATE_PREPARES => false,
     		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     	]);
